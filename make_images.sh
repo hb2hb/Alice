465f4f0908955fbd2c91daa18ecdccc3061298cd
Henry <hb2hb@yandex.ru>
2023-12-01 14:41:48 +0300

    add make_image script

---
+++ make_images.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+# -----------------------------------------
+# project name: Alice
+# created: 2023/12/01
+# authors: H.B. & co.
+# target: create folder structure and
+# copy config files
+# =========================================
+
+
+source ./vars
+
+
